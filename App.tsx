817e9bc8b3d1e1806f8cfbd2aecae27fa647e874
Andres <andres.porfirio.tablet@gmail.com>
2023-09-06 18:03:40 -0300

    example from video

--- App.tsx
+++ App.tsx
@@ -1,11 +1,40 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, View } from "react-native";
+import { Text, Input, Icon, CheckBox, Switch, Button } from "@rneui/themed";
+import { useState } from "react";
 
 export default function App() {
+  const [name, setName] = useState("");
+  const [accept, setAccept] = useState(true);
+
+  const handleSubmit = () => console.log("Send:", name, accept);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text h1>My Form</Text>
+
+      <Input
+        placeholder="Name"
+        leftIcon={<Icon name="user" type="antdesign" />}
+        value={name}
+        onChangeText={setName}
+      />
+
+      <CheckBox
+        checked={accept}
+        onPress={() => {
+          setAccept((prev) => !prev);
+        }}
+        title="Accept Terms"
+      />
+
+      <Switch value={accept} onValueChange={(value) => setAccept(value)} />
+
+      <Button title="Cancel" type="outline" />
+
+      <Button type="solid" onPress={handleSubmit}>
+        Submit
+        <Icon name="send" color="white" type="feather" />
+      </Button>
     </View>
   );
 }
